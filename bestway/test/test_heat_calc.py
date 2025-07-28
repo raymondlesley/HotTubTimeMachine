0507dbabb34e7ff5a12e3c007848d8248e05e90a
raymondlesley <github.raymond@thelesleys.co.uk>
2025-07-28 17:51:49 +0100

    outline tub heating algorithm - original and improved, with unit tests

---
+++ bestway/test/test_heat_calc.py
@@ -0,0 +1,45 @@
+import logging
+from unittest import TestCase
+from random import randint
+
+import log_config
+import tub_utils
+from tub_utils import STEP_RATE
+
+log_config.prepare_logging(logging.INFO)
+
+class TestHeatCalc(TestCase):
+    def test_same_result_1(self):
+        heating = tub_utils.CalcHeatTime_iterate(22 * 60, 36, 38, 350, 40, 9.5 * 60)
+        self.assertEqual(heating.start_time, 440, "start_time incorrect")
+        self.assertEqual(heating.time_to_heat, 130, "time_to_heat incorrect")
+
+    def test_same_result_2(self):
+        heating = tub_utils.CalcHeatTime_algebra(22 * 60, 36, 38, 350, 40, 9.5 * 60)
+        self.assertAlmostEqual(heating.start_time, 440, delta=STEP_RATE)
+        self.assertAlmostEqual(heating.time_to_heat, 130, delta=STEP_RATE)
+
+    def test_random_results(self):
+        for i in range(1000):
+            start_time = 22 * 60 # 22:00
+            start_temp = randint(22, 38)
+            target_temp = randint(30, 40)
+            cool_rate = randint(160, 400)
+            heat_rate = randint(30, 60)
+            time_left = 9.5 * 60  # 07:30 tomorrow
+            logging.info(f"Trying: {start_temp}->{target_temp} at {cool_rate}/{heat_rate}")
+            it_heating = tub_utils.CalcHeatTime_iterate(start_time, start_temp, target_temp, cool_rate, heat_rate, time_left)
+            al_heating = tub_utils.CalcHeatTime_algebra(start_time, start_temp, target_temp, cool_rate, heat_rate, time_left)
+            if (it_heating.start_time is None):
+                try:
+                    self.assertEqual(al_heating.start_time, None)
+                except:
+                    self.assertAlmostEqual(al_heating.start_time, time_left, delta=STEP_RATE)
+            elif (al_heating.start_time is None):
+                try:
+                    self.assertEqual(al_heating.start_time, None)
+                except:
+                    self.assertAlmostEqual(al_heating.start_time, time_left, delta=STEP_RATE)
+            else:
+                self.assertAlmostEqual(it_heating.start_time, al_heating.start_time, delta=STEP_RATE)
+                self.assertAlmostEqual(it_heating.time_to_heat, al_heating.time_to_heat, delta=STEP_RATE)
---
+++ tub_utils.py
@@ -0,0 +1,82 @@
+import logging
+
+from asn1crypto.core import Integer
+
+# import bestway
+
+# ---------------------------------------------------------------------------
+
+ECO_MINS    = 7 * 60
+STEP_RATE   = 10  # minutes per iteration for calculating heat time
+
+from typing import NamedTuple
+
+class tub_heating(NamedTuple):
+    start_time: Integer
+    time_to_heat: Integer
+
+def CalcHeatTime_iterate(start_time, start_temp, target_temp, cool_rate, heat_rate, time_left):
+    logging.info("calculating start and duration (iterative)")
+    time_to_heat = 0
+    tracked_temp = float(start_temp)
+    target_temp = float(target_temp)
+    logging.debug(f"Temp now = {tracked_temp}")
+    start_time = 0
+    logging.debug(f"target temperature {target_temp} in {time_left} minutes")
+
+    while (start_time + time_to_heat) < time_left:
+        # walk forwards from now until the time to start heating is reached
+        start_time += STEP_RATE
+        tracked_temp -= STEP_RATE / cool_rate
+        time_to_heat = int((target_temp - tracked_temp) * heat_rate)
+        logging.debug(f"temp after {start_time} minutes = {tracked_temp:.1f}; {time_to_heat} mins heating needed")
+
+    if time_to_heat > 0:
+        if time_to_heat > ECO_MINS: time_to_heat = ECO_MINS
+        start_time = time_left - time_to_heat
+        logging.info(f"Setting timer to start in {start_time} minutes; heat for {time_to_heat} minutes")
+    elif time_to_heat <= 0:
+        # -ve heating time - indicates will still be > target temperature after cooling
+        start_time = None
+        time_to_heat= None
+        logging.info(f"temperature ({start_temp}) projected to end at {tracked_temp:.1f} - over setpoint {target_temp}")
+
+    return tub_heating(start_time, time_to_heat)
+
+# ---------------------------------------------------------------------------
+
+def CalcHeatTime_algebra(start_time, start_temp, target_temp, cool_rate, heat_rate, time_left):
+    logging.info("calculating start and duration (algebraic)")
+    time_to_heat = 0
+    tracked_temp = float(start_temp)
+    target_temp = float(target_temp)
+    cool_rate = 1.0 / float(cool_rate)
+    heat_rate = 1.0 / float(heat_rate)
+    logging.debug(f"Temp now = {tracked_temp}")
+    start_time = 0
+    logging.debug(f"target temperature {target_temp} in {time_left} minutes")
+
+    # From symbolab.com/solver: heat_time = (Tend - Tstart + cool_rate * total_time) /  (heat_rate + cool_rate)
+    # (See my Google sheet "Hot Hub Heating Timer")
+
+    time_to_heat = (target_temp - tracked_temp + cool_rate * time_left) / (heat_rate + cool_rate)
+    start_time = time_left - time_to_heat
+
+    if time_to_heat > 0:
+        if time_to_heat > ECO_MINS: time_to_heat = ECO_MINS
+        start_time = time_left - time_to_heat
+        logging.info(f"Setting timer to start in {start_time:.1f} minutes; heat for {time_to_heat:.1f} minutes")
+        return tub_heating(int(start_time), int(time_to_heat))
+    elif time_to_heat <= 0:
+        # -ve heating time - indicates will still be > target temperature after cooling
+        start_time = None
+        time_to_heat= None
+        logging.info(f"current temperature ({start_temp}) projected to remain over target {target_temp}")
+        return tub_heating(None, None)
+
+
+# ---------------------------------------------------------------------------
+
+CalcHeatTime = CalcHeatTime_algebra
+
+# ---------------------------------------------------------------------------
